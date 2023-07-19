8f9c2718a2ce20d585ae38abb4aaf9336150d7f0
SOPLAY <tkdgns1341@gmail.com>
2023-07-19 22:02:49 +0900

    ✨Feat: DaisyUI 추가 및 useId hooks, Input, Button components 추가

---
+++ hooks/useId.ts
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
+let idCnt = 0;
+
+export function generateId(prefix = "brewing-id") {
+  idCnt = idCnt + 1;
+  return `${prefix}-${idCnt}`;
+}
+
+export default function useId(prefix?: string) {
+  const [id] = useState(() => generateId(prefix));
+  return id;
+}
